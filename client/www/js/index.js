25be6b185501587b3ea938e5664d6e48e94bba2b
Nizijolas <nicolasdijoud@yahoo.com>
2025-12-01 08:44:58 +0100

    ajout main

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -3,8 +3,9 @@ import main from "./htmlAndLoopHandler.js";
 document.addEventListener('deviceready', () => {
     console.log('Running Cordova app');
     document.getElementById('deviceready')?.classList.add('ready');
+    main();
+
     
 });
 
 // lancement du jeu
-main();
